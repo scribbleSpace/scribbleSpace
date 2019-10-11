a656bec934430b4059f36c2b14b9e4337e5a8e53
Joseph Wolensky <jwastoria86@gmail.com>
2019-10-11 16:23:11 -0400

    webpack operational

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'React';
+import ReactDOM from 'react-dom';
+import Styles from './styles.css';
+
+const Index = () => <div>Hello Brian!!</div>;
+
+ReactDOM.render(<Index />, document.getElementById('root'));
