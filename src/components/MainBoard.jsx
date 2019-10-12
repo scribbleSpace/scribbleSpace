cd0fa717031d85522a41c1854d0c92b8a030d883
Joseph Wolensky <jwastoria86@gmail.com>
2019-10-11 20:45:39 -0400

    Day 1 Fin

---
+++ src/components/MainBoard.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import CanvasDraw from 'react-canvas-draw';
+import Styles from '../styles.css';
+
+class MainBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.saveDrawingData = this.saveableCanvas.bind(this);
+  }
+
+  saveDrawingData(data) {
+    fetch('/save', {
+      headers: { 'Content-type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({ data }),
+    });
+  }
+
+  render() {
+    return (
+      <div className="mainCanvas">
+        <CanvasDraw
+          ref={canvasDraw => {
+            this.saveableCanvas = canvasDraw;
+          }}
+          lazyRadius="1"
+          brushRadius="4"
+          canvasWidth="600px"
+        />
+        <br />
+        <br />
+        <button
+          type="button"
+          onClick={() => {
+            this.saveDrawingData(this.saveableCanvas.getSaveData());
+          }}
+        >
+          Save Me
+        </button>
+      </div>
+    );
+  }
+}
+
+export default MainBoard;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'React';
 import ReactDOM from 'react-dom';
 import Styles from './styles.css';
+import MainBoard from './components/MainBoard.jsx';
 
-const Index = () => <div>Hello Brian!!</div>;
+const Index = () => (
+  <div>
+    <h1 className="logoMain">scribbleSpace</h1>
+    <MainBoard />
+  </div>
+);
 
 ReactDOM.render(<Index />, document.getElementById('root'));
